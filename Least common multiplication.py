155f8e96c24a39cdd907b45d09753eae6babb302
Mehedi Hasan Mir <mdmehedihassanmir@gmail.com>
2024-01-05 10:07:19 +0600

    fixing least common multiplication

--- Least common multiplication.py
+++ Least common multiplication.py
@@ -1,14 +1,15 @@
-def count_LMC(a,b):
-    if a > b:
-        greater = a
+def count_LMC(num1,num2):
+    if num1 > num2:
+        greater = num1
     else:
-        greater = b
+        greater = num2
+    value = greater
     while (True):
-        if ((greater % a == 0)and (greater % b)):
-            LCM = greater
+        if ((greater % num1 == 0)and (greater % num2 == 0)):
+            print(f"The least common value of {num1} and {num2} is {greater}")
             break
-        greater +=1
-    return LCM
+        else:
+            greater = greater + value
 num1 = int(input("Enter the first number: "))
 num2 = int(input("Enter the second number: "))
-print("The least common multiplication",count_LMC(num1,num2))
+count_LMC(num1,num2)
